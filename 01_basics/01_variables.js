18d82f7a25ab7b9ca4e8858b052c8607404d0a53
Aditi-Here <66709120+Aditi-Here@users.noreply.github.com>
2024-07-25 14:11:34 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1233
+let accountEmail = "here@gmail.com"
+var accountPass = "123456"
+
+/* 
+Prefer not to use var
+because of issues in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPass])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version
+// alert(3+3) // we are using node js not browser
+console.log( 3+ 
+    3 ) // code readability should be high
+
+let name = "Aditi"
+let age = 18
+let loggedIn = false
+
+// number => 2^53
+// bigInt
+// string => " " (preferred) or ' '
+// boolean
+// null => standalone value
+// undefined => value not assigned yet
+// symbol => unique
+
+// object
+
+console.log(typeof age);
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
