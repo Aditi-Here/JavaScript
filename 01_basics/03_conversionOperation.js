4f0d8cf21e5378a3dbe7bd81d9feb2d85517f977
Aditi-Here <66709120+Aditi-Here@users.noreply.github.com>
2024-07-25 14:51:45 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+let score = "33"
+
+//console.log(typeof score);
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+
+let scoreCheck = "22abc";
+let valueInNUmberCheck = Number(scoreCheck);
+//console.log(typeof valueInNUmberCheck);
+//console.log(valueInNUmberCheck);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// null => 0
+
+let isLoggedIn ;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+//console.log(booleanIsLoggedIn);
+
+// 1 => true,
+// " " => false
+//"ddyy" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+//console.log(stringNumber);
+
+//    operations
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+//console.log(2+2);
+
+let str1 = "hello";
+let str2 = "aditi";
+
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
