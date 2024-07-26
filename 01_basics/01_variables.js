a45cc3ff8b036a2bab6738c3914c0f8ee426bdc8
Aditi-Here <66709120+Aditi-Here@users.noreply.github.com>
2024-07-26 02:20:21 +0000

    changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,9 +2,12 @@ const accountId = 1233
 let accountEmail = "here@gmail.com"
 var accountPass = "123456"
 
+accountEmail = "fsaf@gmail.com";
+console.log(accountId);
+
 /* 
 Prefer not to use var
 because of issues in block scope and functional scope
 */
 
-console.table([accountId,accountEmail,accountPass])
+//console.table([accountId,accountEmail,accountPass])
